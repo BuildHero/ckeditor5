fa1a8d78bd657975e7e6f7154e84d98869a67eab
Carol Dysart <carol@buildops.com>
2023-06-06 22:10:37 -0700

    chore: test fn

--- packages/ckeditor5-build-decoupled-document/src/ckeditor.js
+++ packages/ckeditor5-build-decoupled-document/src/ckeditor.js
@@ -258,6 +258,61 @@ function CustomImageUploadAdapterPlugin( editor ) {
 	};
 }
 
+function MentionCustomization( editor ) {
+	// The upcast converter will convert <a class="mention" href="" data-user-id="">
+	// elements to the model 'mention' attribute.
+	editor.conversion.for( 'upcast' ).elementToAttribute( {
+		view: {
+			name: 'span',
+			key: 'data-mention',
+			classes: 'mention',
+			attributes: {
+				'data-user-id': true
+			}
+		},
+		model: {
+			key: 'mention',
+			value: viewItem => {
+			// The mention feature expects that the mention attribute value
+			// in the model is a plain object with a set of additional attributes.
+			// In order to create a proper object, use the toMentionAttribute helper method:
+				const mentionAttribute = editor.plugins.get( 'Mention' ).toMentionAttribute( viewItem, {
+				// Add any other properties that you need.
+				} );
+
+				return mentionAttribute;
+			}
+		},
+		converterPriority: 'high'
+	} );
+
+	// Downcast the model 'mention' text attribute to a view <a> element.
+	editor.conversion.for( 'downcast' ).attributeToElement( {
+		model: 'mention',
+		view: ( modelAttributeValue, { writer } ) => {
+		// Do not convert empty attributes (lack of value means no mention).
+			if ( !modelAttributeValue ) {
+				return;
+			}
+
+			return writer.createAttributeElement(
+				'span',
+				{
+					class: 'mention',
+					'data-mention': `[[${ modelAttributeValue.id }]]`
+				},
+				{
+					// Make mention attribute to be wrapped by other attribute elements.
+					priority: 20,
+					// Prevent merging mentions together.
+					id: modelAttributeValue.uid
+				}
+			);
+		},
+		converterPriority: 'high'
+	} );
+}
+
 export default class DecoupledEditor extends DecoupledEditorBase {}
 
 const customColorPalette = [
@@ -326,6 +381,7 @@ DecoupledEditor.builtinPlugins = [
 	ListProperties,
 	MediaEmbed,
 	Mention,
+	MentionCustomization,
 	PageBreak,
 	Pagination,
 	Paragraph,
